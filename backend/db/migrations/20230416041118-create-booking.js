22b8ec569744b95f6c0c555721c4a32247e6ccc8
Vanessa Gonzalez <vxg026@gmail.com>
2023-04-17 14:13:06 -0700

    added associations

---
+++ backend/db/migrations/20230416041118-create-booking.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:"Spots"
+        }
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:"Users"
+        },
+        onDelete: "CASCADE"
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Bookings');
+  }
+};
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -12,10 +12,19 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
-
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
 // Log in
 router.post(
-    '/',
+    '/', validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
       const user = await User.unscoped().findOne({
@@ -75,15 +84,6 @@ router.post(
   );
 
 
-  const validateLogin = [
-    check('credential')
-      .exists({ checkFalsy: true })
-      .notEmpty()
-      .withMessage('Please provide a valid email or username.'),
-    check('password')
-      .exists({ checkFalsy: true })
-      .withMessage('Please provide a password.'),
-    handleValidationErrors
-  ];
+
 
 module.exports = router;
