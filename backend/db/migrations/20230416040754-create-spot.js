aebc29bfa01f2d66890922e1a2db564147c28299
Vanessa Gonzalez <vxg026@gmail.com>
2023-04-17 15:41:51 -0700

    passed in options

--- backend/db/migrations/20230416040754-create-spot.js
+++ backend/db/migrations/20230416040754-create-spot.js
@@ -56,7 +56,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
    down: async (queryInterface, Sequelize) =>{
     options.tableName = "Spots";
--- backend/db/migrations/20230416041118-create-booking.js
+++ backend/db/migrations/20230416041118-create-booking.js
@@ -44,7 +44,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    });
+    }, options);
   },
    down:async(queryInterface, Sequelize) =>{
     options.tableName = "Bookings";
