822692f42faf266b07640a489188b5680b39b4b7
Vanessa Gonzalez <vxg026@gmail.com>
2023-04-20 08:52:22 -0700

    put booking done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js')
+const bookingsRouter = require('./bookings.js')
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
 //   });
@@ -12,6 +13,8 @@ const reviewsRouter = require('./reviews.js')
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/bookings', bookingsRouter)
 
 router.use( '/reviews', reviewsRouter)
 router.use('/session', sessionRouter);
