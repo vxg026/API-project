fb91f25fabf656fbf9964411fb18233a49f93139
Vanessa Gonzalez <vxg026@gmail.com>
2023-05-07 14:12:24 -0700

    finished with spots

--- frontend/src/components/Spots/CurrentUserSpots.js
+++ frontend/src/components/Spots/CurrentUserSpots.js
@@ -1,29 +1,29 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, Fragment } from "react";
-import {getCurrentUserSpots} from '../../store/spots'
+import { getCurrentUserSpots } from '../../store/spots'
 import SpotIndexItem from './SpotIndexItem'
 
 
-const  GetCurrentSpots= ()=>{
+const GetCurrentSpots = () => {
     const dispatch = useDispatch()
-        const spotObj = useSelector(state=> state.spots.allSpots)
-        // console.log("currspotobj", spotObj)
-    const spotList =Object.values(spotObj)
-console.log("spotList -------", spotList)
+    const spotObj = useSelector(state => state.spots.currentUserSpots)
+    // console.log("currspotobj", spotObj)
+    const spotList = Object.values(spotObj)
+    console.log("spotList -------", spotList)
 
     useEffect(() => {
         dispatch(getCurrentUserSpots());
-      }, [dispatch]);
+    }, [dispatch]);
 
-return (<>
-{spotList.length>0 && spotList.map(spot=>(
-<Fragment key={spot.id}>
-<h1>curr</h1>
-{spot.name}
-<SpotIndexItem spot={spot} />
-</Fragment>
-    ))}
-</>
-)
+    return (<>
+        {spotList.length > 0 && spotList.map(spot => (
+            <Fragment key={spot.id}>
+                <h1>curr</h1>
+                {spot.name}
+                <SpotIndexItem spot={spot} />
+            </Fragment>
+        ))}
+    </>
+    )
 }
 export default GetCurrentSpots
